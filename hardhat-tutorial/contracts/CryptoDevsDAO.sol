dfa0002f21e1b4b38a0d5dae2f87a88c9d6f06e1
Shubham Sharma <shubhamku044@gmail.com>
2022-08-18 12:56:32 +0530

    first commit

---
+++ hardhat-tutorial/contracts/CryptoDevsDAO.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// We will add the Interfaces here
+
+contract CryptoDevsDAO is Ownable {
+    // We will write contract code here
+}
